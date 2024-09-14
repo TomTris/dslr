ce82fe83416d7bef12fa201705904ab7138f549f
TomTris <quoctrido.jb@gmail.com>
2024-09-14 20:35:40 +0200

    find a way to display color overlaping

--- ex02/histogram.py
+++ ex02/histogram.py
@@ -2,13 +2,15 @@ import pandas as pd
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-# import matplotlib as mpl
+import matplotlib as mpl
 
 
 def read_data(file_path):
 	with open(file_path) as file:
 		reader = pd.read_csv(file, delimiter=',')
+		print(reader)
 		reader = np.array(reader)
+		print(reader)
 		return reader
 
 
@@ -41,11 +43,54 @@ def print_number_of_student_in_each_house(data_table):
 
 def main() -> None:
 	try:
-		if len(sys.argv) != 2:
-			raise Exception("Usage: python histogram.py [dataset]")
-		data_table = read_data(sys.argv[1])
-		
-			
+		import matplotlib.pyplot as plt
+		import numpy as np
+
+		np.random.seed(0)  # For reproducibility
+
+				# Data for four groups
+		data = pd.read_csv(sys.argv[1])
+		data.loc[:, 'Flying'] = data.loc[:, 'Arithmancy'] - 4000
+		# data = pd(data)
+		# data = 
+		# print(data1[1:, 7:9])
+		# Create histogram
+		plt.figure(figsize=(12, 8))
+
+		# List of courses to plot
+		courses = [
+			'Arithmancy',
+			'Astronomy',
+			'Herbology',
+			'Defense Against the Dark Arts',
+			'Divination',
+			'Muggle Studies',
+			'Ancient Runes',
+			'History of Magic',
+			'Transfiguration',
+			'Potions',
+			'Care of Magical Creatures',
+			'Charms',
+			'Flying'
+		]
+
+		# Colors for histograms
+		colors = plt.cm.viridis(np.linspace(0, 1, len(courses)))
+
+		# Plot histograms for each course
+		for i, course in enumerate(courses):
+			plt.hist(data[course], bins=20, alpha=0.5, label=course, color=colors[i])
+
+		# Add titles and labels
+		plt.title('Overlapping Histograms of Hogwarts Courses')
+		plt.xlabel('Scores')
+		plt.ylabel('Frequency')
+
+		# Add legend
+		plt.legend(loc='upper right')
+
+		# Display plot
+		plt.savefig('/app/ex02/histogram.png')
 
 
 	except Exception as e:
