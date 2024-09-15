4826e5bd73d4da0e84b781e47a7b4e900693a761
TomTris <quoctrido.jb@gmail.com>
2024-09-15 13:14:43 +0200

    have normalized data and display in different blocks

--- ex02/histogram.py
+++ ex02/histogram.py
@@ -1,3 +1,5 @@
+
+import csv 
 import pandas as pd
 import sys
 import numpy as np
@@ -5,21 +7,12 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 
-def read_data(file_path):
-	with open(file_path) as file:
-		reader = pd.read_csv(file, delimiter=',')
-		print(reader)
-		reader = np.array(reader)
-		print(reader)
-		return reader
-
-
 def eprint(*args, **kwargs):
 	print('Error:', *args, file=sys.stderr, **kwargs)
 
 
-
 def print_number_of_student_in_each_house(data_table):
+	data_table = np.array(data_table)
 	houses = data_table[:, 1:2]
 	cnt1 = 0
 	cnt2 = 0
@@ -35,63 +28,66 @@ def print_number_of_student_in_each_house(data_table):
 		elif house == 'Gryffindor':
 			cnt4 += 1
 		else:
-			print(house)
-	print(cnt1)
-	print(cnt2)
-	print(cnt3)
-	print(cnt4)
+			raise Exception("Data base has unexpected house")
+	print("Number of students in each house is:")
+	print("Ravenclaw:", cnt1)
+	print("Slytherin:", cnt2)
+	print("Hufflepuff", cnt3)
+	print("Gryffindor", cnt4)
+	print()
+
+def generate_courses():
+	with open(sys.argv[1]) as file:
+		reader = csv.reader(file, delimiter=',')
+		reader = list(reader)
+		for i, row in enumerate(reader):
+			if i > 0:
+				break
+			reader = np.array(row)
+			return reader[6:]
+
+def normalize_score(data, courses):
+	for course in courses:
+		old_max = data[course].max()
+		old_min = data[course].min()
+		data[course] = (data[course] - old_min) / (old_max - old_min)
 
 def main() -> None:
 	try:
-		import matplotlib.pyplot as plt
-		import numpy as np
-
-		np.random.seed(0)  # For reproducibility
-
-				# Data for four groups
 		data = pd.read_csv(sys.argv[1])
-		data.loc[:, 'Flying'] = data.loc[:, 'Arithmancy'] - 4000
-		# data = pd(data)
-		# data = 
-		# print(data1[1:, 7:9])
-		# Create histogram
+		print_number_of_student_in_each_house(data)
+		courses = generate_courses()
+		normalize_score(data, courses)
+		data_1 = data[data['Hogwarts House'] == 'Ravenclaw']
+		data_2 = data[data['Hogwarts House'] == 'Slytherin']
+		data_3 = data[data['Hogwarts House'] == 'Gryffindor']
+		data_4 = data[data['Hogwarts House'] == 'Hufflepuff']
+		
+
 		plt.figure(figsize=(12, 8))
 
-		# List of courses to plot
-		courses = [
-			'Arithmancy',
-			'Astronomy',
-			'Herbology',
-			'Defense Against the Dark Arts',
-			'Divination',
-			'Muggle Studies',
-			'Ancient Runes',
-			'History of Magic',
-			'Transfiguration',
-			'Potions',
-			'Care of Magical Creatures',
-			'Charms',
-			'Flying'
-		]
+		num_courses = len(courses)
+		ncols = 4
+		nrows = (num_courses - 1 ) // ncols + 1
+		fig, axs = plt.subplots(nrows, ncols, figsize=(15, 12))
+		axs = axs.flatten()
+		colors = plt.cm.viridis(np.linspace(0, 1, 4))
 
-		# Colors for histograms
-		colors = plt.cm.viridis(np.linspace(0, 1, len(courses)))
-
-		# Plot histograms for each course
 		for i, course in enumerate(courses):
-			plt.hist(data[course], bins=20, alpha=0.5, label=course, color=colors[i])
+			axs[i].hist(data_1[course], bins=20, alpha=0.55, color=colors[0])
+			axs[i].hist(data_2[course], bins=20, alpha=0.55, color=colors[1])
+			axs[i].hist(data_3[course], bins=20, alpha=0.55, color=colors[2])
+			axs[i].hist(data_4[course], bins=20, alpha=0.55, color=colors[3])
 
-		# Add titles and labels
-		plt.title('Overlapping Histograms of Hogwarts Courses')
-		plt.xlabel('Scores')
-		plt.ylabel('Frequency')
+			axs[i].set_title(course)
+			axs[i].set_xlabel('Normalized Scores')
+			axs[i].set_ylabel('Frequency')
 
-		# Add legend
-		plt.legend(loc='upper right')
+		for j in range(len(courses), len(axs)):
+			fig.delaxes(axs[j])
 
-		# Display plot
-		plt.savefig('/app/ex02/histogram.png')
-
+		plt.tight_layout()
+		plt.savefig('/app/ex02/histograms.png')
 
 	except Exception as e:
 		eprint(e)
